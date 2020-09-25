39253bce22ee26fe5fb48f249a183cdadb551bcc
sdenec <hallo@stefandenecke.de>
2020-09-25 16:16:04 +0200

    Update itemMacro_tollTheDead.js

--- itemMacro_tollTheDead.js
+++ itemMacro_tollTheDead.js
@@ -17,43 +17,43 @@ let hurtDmg = [[hurtDie,dmgType]];
 let targets = Array.from(game.user.targets);
 
 if(targets.length == 0 || targets.length > 1){
-	ui.notifications.warn("Please select a single target to attack.");
+  ui.notifications.warn("Please select a single target to attack.");
 } else {
 
-	let target = targets[0].actor.data;
-	let targetCurrentHealth = target.data.attributes.hp.value;
-	let targetMaxHealth = target.data.attributes.hp.max;
+  let target = targets[0].actor.data;
+  let targetCurrentHealth = target.data.attributes.hp.value;
+  let targetMaxHealth = target.data.attributes.hp.max;
 
-	if(targetCurrentHealth < targetMaxHealth){
-		// print out chat message for hurt state
-			ChatMessage.create({
-				speaker: {
-					alias: currentActor.data.name
-				},
-				content: `
-					<p><b>Toll the dead: Enemy is hurt!</b><br>
-					Using d12 Base Damage!</>
-				`
-			});
-			item.update({
-				'data.damage.parts': hurtDmg,
-				'data.scaling.formula': hurtDie
-			});
+  if(targetCurrentHealth < targetMaxHealth){
+    // print out chat message for hurt state
+      ChatMessage.create({
+        speaker: {
+          alias: currentActor.data.name
+        },
+        content: `
+          <p><b>Toll the dead: Enemy is hurt!</b><br>
+          Using d12 Base Damage!</>
+        `
+      });
+      item.update({
+        'data.damage.parts': hurtDmg,
+        'data.scaling.formula': hurtDie
+      });
 
-			game.dnd5e.rollItemMacro(itemName);
+      game.dnd5e.rollItemMacro(itemName);
 
-			// reset damage die after 500 ms
-			setTimeout(function(){ 
-					item.update({
-						'data.damage.parts': unhurtDmg,
-						'data.scaling.formula': unhurtDie
-					});
-				}, 500);
+      // reset damage die after 500 ms
+      setTimeout(function(){ 
+          item.update({
+            'data.damage.parts': unhurtDmg,
+            'data.scaling.formula': unhurtDie
+          });
+        }, 500);
 
-			// stop execution
-			return;
-	}
+      // stop execution
+      return;
+  }
 
-	// if not favorite enemy do standard roll
-	game.dnd5e.rollItemMacro(itemName);
+  // if not favorite enemy do standard roll
+  game.dnd5e.rollItemMacro(itemName);
 }
