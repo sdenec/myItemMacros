c5c3c295725611f156439d1feca4ff314aac7950
sdenec <hallo@stefandenecke.de>
2020-09-25 16:16:24 +0200

    Update itemMacro_advRanger-favoriteEnemies.js

--- itemMacro_advRanger-favoriteEnemies.js
+++ itemMacro_advRanger-favoriteEnemies.js
@@ -24,40 +24,40 @@ let newDmg = [[newDmgFormula,defaultDmgType]];
 let targets = Array.from(game.user.targets);
 
 if(targets.length == 0 || targets.length > 1){
-	ui.notifications.warn("Please select a single target to attack.");
+  ui.notifications.warn("Please select a single target to attack.");
 } else {
 
-	let target = targets[0].actor.data;
-	let targetType = target.data.details.type;
+  let target = targets[0].actor.data;
+  let targetType = target.data.details.type;
 
-	// if favorite enemy apply bonus
-	for (let i=0, max = favEnemies.length; i < max; i++){
-		if(targetType.toLowerCase() == favEnemies[i]){
+  // if favorite enemy apply bonus
+  for (let i=0, max = favEnemies.length; i < max; i++){
+    if(targetType.toLowerCase() == favEnemies[i]){
 
-			// print out chat message for favorite enemy attack
-			ChatMessage.create({
-				speaker: {
-					alias: currentActor.data.name
-				},
-				content: `
-					<p><b>Attacking Favorite Enemy: <i>${favEnemies[i]}</i></b><br>
-					Adding +${dmgBonus} Damage Bonus on a hit!</>
-				`
-			});
+      // print out chat message for favorite enemy attack
+      ChatMessage.create({
+        speaker: {
+          alias: currentActor.data.name
+        },
+        content: `
+          <p><b>Attacking Favorite Enemy: <i>${favEnemies[i]}</i></b><br>
+          Adding +${dmgBonus} Damage Bonus on a hit!</>
+        `
+      });
 
-			item.update({'data.damage.parts': newDmg});
-			game.dnd5e.rollItemMacro(itemName);
+      item.update({'data.damage.parts': newDmg});
+      game.dnd5e.rollItemMacro(itemName);
 
-			// reset bonus after 500 ms
-			setTimeout(function(){ 
-					item.update({'data.damage.parts': defaultDmg});
-				}, 500);
+      // reset bonus after 500 ms
+      setTimeout(function(){ 
+          item.update({'data.damage.parts': defaultDmg});
+        }, 500);
 
-			// stop execution
-			return;
-		}
-	}
+      // stop execution
+      return;
+    }
+  }
 
-	// if not favorite enemy do standard roll
-	game.dnd5e.rollItemMacro(itemName);
+  // if not favorite enemy do standard roll
+  game.dnd5e.rollItemMacro(itemName);
 }
